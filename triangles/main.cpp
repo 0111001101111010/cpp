f2324a26a66603e7350b7a5d299c69f08f2ebf11
Stanzheng <szhen002@odu.edu>
2013-09-10 13:21:35 -0400

    350 assign 1

---
+++ triangles/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char **argv){
+
+int a1= 0, a2= 0, a3= 0, b1= 0, b2= 0, b3= 0, c1= 0, c2= 0, c3=0;
+
+//enter three points or take from input
+	//no input detected 
+	cout << "please enter first coordinates in ' X Y Z 'format \n" ;
+	cin >> a1 >> a2 >> a3;
+	cout << "please enter second coordinates in ' X Y Z 'format \n" ;
+	cin >> a1 >> a2 >> a3;
+	cout << "please enter third coordinate in ' X Y Z 'format \n" ;
+	cin >> a1 >> a2 >> a3;
+	//operate on these points A,B,C
+		//figure out length of A->B, B->C, C->A
+		//determine if colinear
+		//determine type of triangle
+			//iscosoles
+			//scalene
+			//equalateral
+
+}
