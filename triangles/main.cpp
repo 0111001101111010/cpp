517ba54f6bef3c1f3e18b6e60d8f48e5282eddaf
stanley zheng <szhen002@odu.edu>
2013-09-10 23:49:01 -0400

    added progress

--- triangles/main.cpp
+++ triangles/main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int main(int argc, char **argv){
 
 int a1= 0, a2= 0, a3= 0, b1= 0, b2= 0, b3= 0, c1= 0, c2= 0, c3=0;
-
+double x=0, y=0, z=0; 
+bool continue = false, triangle = false; 
 //enter three points or take from input
 	//no input detected 
 	cout << "please enter first coordinates in ' X Y Z 'format \n" ;
@@ -17,10 +18,17 @@ int a1= 0, a2= 0, a3= 0, b1= 0, b2= 0, b3= 0, c1= 0, c2= 0, c3=0;
 	cin >> a1 >> a2 >> a3;
 	//operate on these points A,B,C
 		//figure out length of A->B, B->C, C->A
+
+	//calculating A->B
+	x = sqrt(pow(a1-b1,2)+ pow(a2-b2,2) + pow(a3-b3,2));  
+	y = sqrt(pow(b1-c1,2)+ pow(b2-c2,2) + pow(b3-c3,2));
+	z = sqrt(pow(c1-a1,2)+ pow(c2,a2,2) = pow(c3-a3,2));	
+		//rule of lines
 		//determine if colinear
 		//determine type of triangle
 			//iscosoles
 			//scalene
 			//equalateral
 
+	
 }
