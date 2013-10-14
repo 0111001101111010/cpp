7b36ed3c7b2aba90c7a6526266eb66f0e4ca12c4
Stanzheng <szhen002@odu.edu>
2013-10-14 18:50:43 -0400

    saved progress

---
+++ class/q1.c
@@ -0,0 +1,13 @@
+do {
+flag[i] = TRUE;
+while (flag[j]) {
+if (turn == j) {
+flag[i] = false;
+while (turn == j){
+// do nothing
+flag[i] = TRUE;
+}
+} // critical section
+turn = flag[i] = FALSE;
+// remainder section
+} while (TRUE);
---
+++ warmachine/dice.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+/*!
+Dice Roll Module
+=================
+Given number of dice, will roll x number of 6 sided dice 
+
+*/
+
+int main(int argc, char** argv){
+
+/*
+Specification of Dice
+---------------------
+Given number of sides 	
+Given number of dice rolls 
+Be able to pass back 
+*/
+
+int rand_num = rand() ;
+
+return 0; 	
+}
