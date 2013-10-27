10ceb00ce05f6665500aeadce106d399bbd38ab0
stanley zheng <szhen002@odu.edu>
2013-10-27 16:02:35 -0400

    added more documents

---
+++ warmachine/Welcome to WARMACHINE   Privateer Press_files/suckerfish.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  // Hover emulation for IE 6.
+  if ($.browser.msie && parseInt(jQuery.browser.version) == 6) {
+    $('#suckerfish li').hover(function() {
+      $(this).addClass('iehover');
+    }, function() {
+      $(this).removeClass('iehover');
+    });
+  }
+
+  // Delayed mouseout.
+  $('#suckerfish li').hover(function() {
+    // Stop the timer.
+    clearTimeout(this.sfTimer);
+    // Display child lists.
+    $('> ul', this).css({left: 'auto', display: 'block'})
+      // Immediately hide nephew lists.
+      .parent().siblings('li').children('ul').css({left: '-999em', display: 'none'});
+  }, function() {
+    // Start the timer.
+    var uls = $('> ul', this);
+    this.sfTimer = setTimeout(function() {
+      uls.css({left: '-999em', display: 'none'});
+    }, 1000);
+  });
+});
